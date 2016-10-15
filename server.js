c77f0fb79093182f3ae639574b84394e0448c8b2
Neence <neencenator@gmail.com>
2016-10-15 12:35:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     articleName=req.params.articleName;
-   // document.write($articleName);
+    console.log($articleName);
       res.send(createTemplate(articles[articleName]));
 });
 
